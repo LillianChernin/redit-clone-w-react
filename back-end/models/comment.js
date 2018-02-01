3084da6f443b4958b50e9a0650af45f2fdde8f37
Lillian Chernin <l.smith1654@gmail.com>
2018-01-31 22:17:53 -0600

    create textPost and comment models

--- back-end/models/comment.js
+++ back-end/models/comment.js
@@ -1,1 +1,16 @@
 // Add your comment model here
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema ({
+  userName: String,
+  text: String,
+  timePosted: Date,
+  upVotes: Number,
+  downVotes: Number
+})
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment;
+module.exports.Schema =  commentSchema;
